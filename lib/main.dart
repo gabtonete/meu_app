66ba82cc4b346a9111dea3724fd09128e0f33b00
Gabriel Tonete <gabton77@hotmail.com>
2023-12-27 23:31:17 -0300

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
   const Task(this.nome, {Key? key}) : super(key: key);
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
   Widget build(BuildContext context) {
-    int nivel = 0;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Stack(
@@ -69,14 +75,16 @@ class Task extends StatelessWidget {
                     Container(
                       width: 200,
                       child: Text(
-                        nome,
+                        widget.nome,
                         style: TextStyle(fontSize: 24),
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
                     ElevatedButton(
                         onPressed: () {
-                          nivel++;
+                          setState(() {
+                            nivel++;
+                          });
                           print(nivel);
                         },
                         child: Icon(Icons.arrow_drop_up))
