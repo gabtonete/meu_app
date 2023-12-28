f4d0364ed84602a7f07b0f6cc2dbefa6325b14a3
Gabriel Tonete <gabton77@hotmail.com>
2023-12-27 22:09:36 -0300

    estrutura tela flutter

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Container(
-        color: Colors.black,
+        color: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Stack(
+              alignment: AlignmentDirectional.center,
               children: [
                 Container(
                   color: Colors.red,
@@ -29,74 +30,65 @@ class MyApp extends StatelessWidget {
                 ),
                 Container(
                   color: Colors.blue,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.indigo,
-                  width: 100,
-                  height: 100,
+                  width: 50,
+                  height: 50,
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Stack(
+              alignment: AlignmentDirectional.center,
               children: [
-                Container(
-                  color: Colors.amberAccent,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.lightGreen,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.teal,
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
                 Container(
                   color: Colors.blue,
                   width: 100,
                   height: 100,
                 ),
                 Container(
-                  color: Colors.indigo,
-                  width: 100,
-                  height: 100,
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
                 ),
               ],
             ),
-            // Container(
-            //   color: Colors.amber,
-            //   height: 30,
-            //   width: 300,
-            //   child: const Text(
-            //     'Teste',
-            //     style: TextStyle(
-            //       color: Colors.black,
-            //       fontSize: 28,
-            //     ),
-            //     textAlign: TextAlign.center,
-            //   ),
-            // ),
-            // ElevatedButton(
-            //     onPressed: () {
-            //       print('Você apertou o botão');
-            //     },
-            //     child: Text('Teste'))
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Diamante Bruto',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o botão');
+                },
+                child: Text('Botãozinho'))
           ],
         ),
       ),
